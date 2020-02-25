26728af20f57089d90c79aa2312b75c681f20512
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-02-25 16:31:44 +0000

    creating dataloader

--- src/main/java/com/codeclan/example/files_and_folders/components/DataLoader.java
+++ src/main/java/com/codeclan/example/files_and_folders/components/DataLoader.java
@@ -1,4 +1,33 @@
 package com.codeclan.example.files_and_folders.components;
 
-public class DataLoader {
+import com.codeclan.example.files_and_folders.models.Folder;
+import com.codeclan.example.files_and_folders.models.User;
+import com.codeclan.example.files_and_folders.repositories.FileRepository;
+import com.codeclan.example.files_and_folders.repositories.FolderRepository;
+import com.codeclan.example.files_and_folders.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    FileRepository fileRepository;
+    @Autowired
+    FolderRepository folderRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    public DataLoader() {
+    }
+
+    public void run(ApplicationArguments args) {
+        User andy = new User("Andy");
+        userRepository.save(andy);
+
+        Folder stuff = new Folder("Stuff", andy);
+        folderRepository.save(stuff);
+    }
 }
